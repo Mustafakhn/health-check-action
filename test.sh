106f61ece90112658c528ca1e50492b3ddbf107a
Mustafakhn <91662007+Mustafakhn@users.noreply.github.com>
2022-12-25 18:27:04 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -19,8 +19,9 @@ then
       curl -v --silent $1 2>&1 | grep -Po $2
 #       wget $1 -q -O - | grep -Po $2
       status=$(echo $?)
-   done
+  done
 elif [[ "$3" != "curl" ]]
+then
   while [ "$status" != "0" ]; do
       sleep 5
 #       curl $1
@@ -28,14 +29,14 @@ elif [[ "$3" != "curl" ]]
 #       curl -v --silent $1 2>&1
       wget $1 -q -O -
       status=$(echo $?)
-   done
+  done
 else
-   while [ "$status" != "0" ]; do
+  while [ "$status" != "0" ]; do
       sleep 5
       curl $1
 #       wget -S --spider $1 2>&1 | grep HTTP/
       curl -v --silent $1 2>&1
 #       wget $1 -q -O -
       status=$(echo $?)
-   done
+  done
 fi
