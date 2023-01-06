74462834aad8c93cd6eaa0e1680ed13785d61173
Mustafa Aamir Khan <91662007+Mustafakhn@users.noreply.github.com>
2023-01-06 19:05:01 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 
 if [[ "$2" != "none" && "$3" != "curl" ]]
 then
+  sudo apt-get install wget
   while [ "$status" != "0" ]; do
       sleep 5
       wget -S --no-cache --spider $1 2>&1 | grep HTTP/
@@ -10,6 +11,7 @@ then
   done
 elif [[ "$2" != "none" ]]
 then
+  sudo apt-get install curl
   while [ "$status" != "0" ]; do
       sleep 5
       curl $1
@@ -18,6 +20,7 @@ then
   done
 elif [[ "$3" != "curl" ]]
 then
+  sudo apt-get install wget
   while [ "$status" != "0" ]; do
       sleep 5
       wget -S --no-cache --spider $1 2>&1 | grep HTTP/
@@ -25,6 +28,7 @@ then
       status=$(echo $?)
   done
 else
+  sudo apt-get install curl
   while [ "$status" != "0" ]; do
       sleep 5
       curl $1
