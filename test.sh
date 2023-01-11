0156bff1ea69223dd554bff9c7a8f5a9d86fcbc7
Mustafa Aamir Khan <91662007+Mustafakhn@users.noreply.github.com>
2023-01-11 15:45:36 +0530

    made the test.sh more readable

--- test.sh
+++ test.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-if [[ "$2" != "none" && "$3" != "curl" ]]
+website=$1
+version=$2
+method=$3
+
+if [[ "$version" != "none" && "$method" != "curl" ]]
 then
   sudo apt-get install wget
   while [ "$status" != "0" ]; do
       sleep 5
-      wget -S --no-cache --spider $1 2>&1 | grep HTTP/
-      wget $1 -q -O - | grep -Po $2
+      wget -S --no-cache --spider $website 2>&1 | grep HTTP/
+      wget $website -q -O - | grep -Po $version
       status=$(echo $?)
   done
-elif [[ "$2" != "none" ]]
+elif [[ "$version" != "none" ]]
 then
   sudo apt-get install curl
   while [ "$status" != "0" ]; do
       sleep 5
-      curl $1
-      curl -H 'Cache-Control: no-cache, no-store' -v --silent $1 2>&1 | grep -Po $2
+      curl $website
+      curl -H 'Cache-Control: no-cache, no-store' -v --silent $website 2>&1 | grep -Po $version
       status=$(echo $?)
   done
-elif [[ "$3" != "curl" ]]
+elif [[ "$method" != "curl" ]]
 then
   sudo apt-get install wget
   while [ "$status" != "0" ]; do
       sleep 5
-      wget -S --no-cache --spider $1 2>&1 | grep HTTP/
-      wget $1 -q -O -
+      wget -S --no-cache --spider $website 2>&1 | grep HTTP/
+      wget $website -q -O -
       status=$(echo $?)
   done
 else
   sudo apt-get install curl
   while [ "$status" != "0" ]; do
       sleep 5
-      curl $1
-      curl -H 'Cache-Control: no-cache, no-store' -v --silent $1 2>&1
+      curl $website
+      curl -H 'Cache-Control: no-cache, no-store' -v --silent $website 2>&1
       status=$(echo $?)
   done
 fi
